c8286ea051f46a818fb94b975e8f95f5707a63fc
Paul Hammer <phammer@umd.edu>
2015-10-08 16:14:38 -0400

    LIBWEB-2662.  Fixed formatting
    https://issues.umd.edu/browse/LIBWEB-2662

--- src/main/java/edu/umd/lib/hippo/update/PublicUpdater.java
+++ src/main/java/edu/umd/lib/hippo/update/PublicUpdater.java
@@ -15,63 +15,62 @@ import org.onehippo.cms7.repository.migration.BasicUpdater;
  */
 public class PublicUpdater extends BasicUpdater {
 
-	@Override
-	public void update(final Node node) throws RepositoryException {
-		final String primaryNodeTypeName = node.getPrimaryNodeType().getName();
-		if (primaryNodeTypeName.equals(getOldNamespacePrefix()
-				+ ":htmlfragmentblock")) {
+  @Override
+  public void update(final Node node) throws RepositoryException {
+    final String primaryNodeTypeName = node.getPrimaryNodeType().getName();
+    if (primaryNodeTypeName.equals(getOldNamespacePrefix()
+        + ":htmlfragmentblock")) {
 
-			/*
-			 * Relaxed nodes may not be unstructured So, if it is not relaxed,
-			 * then make it unstructured
-			 */
-			boolean notRelaxed = false;
-			if (node.isNodeType("hippostd:relaxed")) {
-				System.out.println("Has Relaxed");
+      /*
+       * Relaxed nodes may not be unstructured So, if it is not relaxed, then
+       * make it unstructured
+       */
+      boolean notRelaxed = false;
+      if (node.isNodeType("hippostd:relaxed")) {
+        System.out.println("Has Relaxed");
 
-			} else {
-				System.out.println("Not Relaxed");
-				notRelaxed = true;
-				node.addMixin("hipposys:unstructured");
-			}
+      } else {
+        System.out.println("Not Relaxed");
+        notRelaxed = true;
+        node.addMixin("hipposys:unstructured");
+      }
 
-			/*
-			 * Fields that will be illegal after the migration have to be
-			 * removed at this point, if they exist.
-			 */
-			if (node.hasProperty("hippo:related")) {
-				node.getProperty("hippo:related").remove();
-			}
+      /*
+       * Fields that will be illegal after the migration have to be removed at
+       * this point, if they exist.
+       */
+      if (node.hasProperty("hippo:related")) {
+        node.getProperty("hippo:related").remove();
+      }
 
-			/*
-			 * Set the primary type to the new cnd being imported
-			 */
-			node.setPrimaryType(getNewNamespacePrefix() + ":htmlfragmentblock");
+      /*
+       * Set the primary type to the new cnd being imported
+       */
+      node.setPrimaryType(getNewNamespacePrefix() + ":htmlfragmentblock");
 
-			/*
-			 * If we added the unstructured mixin earlier, remove it now.
-			 */
-			if (notRelaxed) {
-				node.removeMixin("hipposys:unstructured");
-			}
+      /*
+       * If we added the unstructured mixin earlier, remove it now.
+       */
+      if (notRelaxed) {
+        node.removeMixin("hipposys:unstructured");
+      }
 
-		}
+    }
 
-		if (primaryNodeTypeName.equals(getOldNamespacePrefix() + ":bookmark")) {
+    if (primaryNodeTypeName.equals(getOldNamespacePrefix() + ":bookmark")) {
 
-			// Add any missing properties
-			if (!node.hasProperty(getNewNamespacePrefix() + ":username")) {
-				node.setProperty(getNewNamespacePrefix() + ":username",
-						"phammer");
-			}
+      // Add any missing properties
+      if (!node.hasProperty(getNewNamespacePrefix() + ":username")) {
+        node.setProperty(getNewNamespacePrefix() + ":username", "phammer");
+      }
 
-		}
-		super.update(node);
-	}
+    }
+    super.update(node);
+  }
 
-	public static void main(String[] args) {
-		PublicUpdater updater = new PublicUpdater();
-		updater.main(args);
-	}
+  public static void main(String[] args) {
+    PublicUpdater updater = new PublicUpdater();
+    updater.main(args);
+  }
 
 }
